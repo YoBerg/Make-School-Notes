68101dd7a665aa2d1d868c2d9c157db7d067e0fc
Yohan Berg <yohan520@gmail.com>
2018-07-06 09:56:48 -0700

    Completed Project Added
    First commit of Make School Notes. This commit adds the entire finished app.

---
+++ MakeSchoolNotes-Swift-V2-Starter-swift4-coredata/MakeSchoolNotes/Controllers/ListNotesTableViewController.swift
@@ -0,0 +1,89 @@
+//
+//  ListNotesTableViewController.swift
+//  MakeSchoolNotes
+//
+//  Created by Chris Orcutt on 1/10/16.
+//  Copyright © 2016 MakeSchool. All rights reserved.
+//
+
+import UIKit
+
+class ListNotesTableViewController: UITableViewController {
+    
+    @IBAction func unwindWithSegue(_ segue: UIStoryboardSegue) {
+        let tempNotes = CoreDataHelper.retrieveNotes()
+        notes = []
+        for tempNote in tempNotes {
+            notes.insert(tempNote, at: 0)
+        }
+    }
+    
+    var notes = [Note]() {
+        didSet {
+            tableView.reloadData()
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            let noteToDelete = notes[indexPath.row]
+            CoreDataHelper.delete(note: noteToDelete)
+            
+            let tempNotes = CoreDataHelper.retrieveNotes()
+            notes = []
+            for tempNote in tempNotes {
+                notes.insert(tempNote, at: 0)
+            }
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let tempNotes = CoreDataHelper.retrieveNotes()
+        notes = []
+        for tempNote in tempNotes {
+            notes.insert(tempNote, at: 0)
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // 1
+        return notes.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "listNotesTableViewCell", for: indexPath) as! ListNotesTableViewCell
+        
+        let note = notes[indexPath.row]
+        cell.noteTitleLabel.text = note.title
+        // 1
+        cell.noteModificationTimeLabel.text = note.modificationTime?.convertToString() ?? "unknown"
+        cell.previewTextBox.text = note.content
+        
+        return cell
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let identifier = segue.identifier else { return }
+        
+        switch identifier {
+        case "displayNote":
+            // 1
+            guard let indexPath = tableView.indexPathForSelectedRow else { return }
+            
+            // 2
+            let note = notes[indexPath.row]
+            // 3
+            let destination = segue.destination as! DisplayNoteViewController
+            // 4
+            destination.note = note
+            
+        case "addNote":
+            print("create note bar button item tapped")
+            
+        default:
+            print("unexpected segue identifier")
+        }
+    }
+}
